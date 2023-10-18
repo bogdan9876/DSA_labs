9c72a04b00345575d1a7d784436b0d7137db9d18
Bogdan <119266071+bogdan9876@users.noreply.github.com>
2023-10-18 17:42:32 +0300

    Update test_binary_tree.py

--- lab3.lvl2/test/test_binary_tree.py
+++ lab3.lvl2/test/test_binary_tree.py
@@ -4,14 +4,14 @@ from src.binary_tree import BinaryTree, invert_binary_tree
 
 class TestInvertBinaryTree(unittest.TestCase):
     def test_invert_binary_tree(self):
-        tree = BinaryTree(1)
-        tree.left = BinaryTree(2)
-        tree.right = BinaryTree(3)
+        tree = BinaryTree(3)
+        tree.left = BinaryTree(9)
+        tree.right = BinaryTree(20)
         inverted_tree = invert_binary_tree(tree)
 
-        self.assertEqual(inverted_tree.value, 1)
-        self.assertEqual(inverted_tree.left.value, 3)
-        self.assertEqual(inverted_tree.right.value, 2)
+        self.assertEqual(inverted_tree.value, 3)
+        self.assertEqual(inverted_tree.left.value, 20)
+        self.assertEqual(inverted_tree.right.value, 9)
 
 
 if __name__ == '__main__':
